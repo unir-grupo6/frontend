aa212943b4380a9573a47e3d580772a8722af689
andresmir-94 <andresmirsampedro@gmail.com>
2025-06-05 19:59:43 +0200

    Rutas realizadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { Error404Component } from './pages/error404/error404.component';
+import { InfoComponent } from './pages/info/info.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path: 'home', component: HomeComponent}, 
+    {path: 'login', component: LoginComponent},
+    {path: 'register', component: RegisterComponent},
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'info', component: InfoComponent},
+    {path: '**', component: Error404Component}
+];
