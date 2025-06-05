15f5a795337d14b9abfba6b912357eb35b1c1dc7
andresmir-94 <andresmirsampedro@gmail.com>
2025-06-05 19:04:27 +0200

    Proyecto iniciado

---
+++ src/app/components/button-component/button-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-component',
+  imports: [],
+  templateUrl: './button-component.component.html',
+  styleUrl: './button-component.component.css'
+})
+export class ButtonComponentComponent {
+
+}
---
+++ src/app/components/card-component/card-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-component',
+  imports: [],
+  templateUrl: './card-component.component.html',
+  styleUrl: './card-component.component.css'
+})
+export class CardComponentComponent {
+
+}
