952fcc8e0221e5475c82848169fabe86a10dade2
andresmir-94 <andresmirsampedro@gmail.com>
2025-06-06 21:11:29 +0200

    nav y footer

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  menuAbierto: boolean = false;
 
+  toggleMenu() {
+    this.menuAbierto = !this.menuAbierto;
+  }
 }
