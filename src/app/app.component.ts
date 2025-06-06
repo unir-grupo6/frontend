122ae18ffbbae38eb2799b349ef4658e1f91f3ff
Daniel A. <1danielansias@gmail.com>
2025-06-06 16:34:34 +0200

    estilos globales y setup de flowbite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rutinaGo';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
