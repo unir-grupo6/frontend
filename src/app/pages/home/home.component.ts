cc828256d2190aea39de2aef8dad149d4d1f368e
Daniel A. <1danielansias@gmail.com>
2025-06-08 12:18:15 +0200

    Features section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../../shared/footer/footer.component";
 import { NavComponent } from "../../shared/nav/nav.component";
+import { CardComponentComponent } from '../../components/card-component/card-component.component';
 
 @Component({
   selector: 'app-home',
-  imports: [FooterComponent, NavComponent],
+  imports: [FooterComponent, NavComponent, CardComponentComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
