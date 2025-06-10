b2bfc86704e9c904c96c252987160208ef71f92d
andresmir-94 <andresmirsampedro@gmail.com>
2025-06-10 22:16:20 +0200

    Estructura y diseño del login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FooterComponent } from "../../shared/footer/footer.component";
+import { NavComponent } from "../../shared/nav/nav.component";
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterLink, FooterComponent, NavComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
