3cac8d6ea950a46d631fdbb99107000e98660f50
ronito19 <ronnie_1019@hotmail.com>
2025-06-11 19:04:29 +0200

    Inicio de Formulario de registro

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  menuAbierto: boolean = false;
 
+  toggleMenu() {
+    this.menuAbierto = !this.menuAbierto;
+  }
 }
+
