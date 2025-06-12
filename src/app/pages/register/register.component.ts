9edd2bb1b9ddb8a475b173e9d2d638c6532e47e8
ronito19 <ronnie_1019@hotmail.com>
2025-06-12 19:39:45 +0200

    Creacion de RouterLink para los botones de Inicio y el logo

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,16 +1,40 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
   menuAbierto: boolean = false;
+  dias: number[] = [];
+  años: number[] = [];
+
+  ngOnInit() {
+    this.generarDias();
+    this.generarAños();
+  }
 
   toggleMenu() {
     this.menuAbierto = !this.menuAbierto;
   }
+
+  generarDias() {
+    for (let i = 1; i <= 31; i++) {
+      this.dias.push(i);
+    }
+  }
+
+  generarAños() {
+    const añoActual = new Date().getFullYear();
+    const añoInicio = añoActual - 100; //Opcion: desde hace 100 años
+    for (let i = añoActual; i >= añoInicio; i--) {
+      this.años.push(i);
+    } 
+   }
+
+
 }
 
