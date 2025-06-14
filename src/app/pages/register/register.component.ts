56d119480afe4aae4b2a43d45d9726e3cb14e56e
ronito19 <ronnie_1019@hotmail.com>
2025-06-14 15:03:00 +0200

    Introduciendo el icono de ocultar contraseña en password y en repetir password

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-register',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
   menuAbierto: boolean = false;
+  password: string = '';
+  confirmPassword: string = '';
+  showPassword: boolean = false;
+  showConfirmPassword: boolean = false;
   dias: number[] = [];
   anios: number[] = [];
 
@@ -27,6 +32,7 @@ export class RegisterComponent {
     }
   }
 
+
   generarAños() {
     const añoActual = new Date().getFullYear();
     const añoInicio = añoActual - 100; //Opcion: desde hace 100 años
@@ -36,7 +42,25 @@ export class RegisterComponent {
    }
 
 
-   
+  get passwordsMatch(): boolean {
+    return this.password === this.confirmPassword; // Devuelve true si las contraseñas coinciden
+  }
+
+
+  togglePasswordVisibility() {
+      this.showPassword = !this.showPassword;
+    }
+
+
+  toggleConfirmPasswordVisibility() {
+    this.showConfirmPassword = !this.showConfirmPassword;
+  }
+
+  
+  // Mensaje de error
+  errorMessage: string = "";
+
+  
 
 }
 
