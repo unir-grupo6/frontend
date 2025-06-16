09d63b52d7d04b0bbb7191e9e0c4a627e9583d07
andresmir-94 <andresmirsampedro@gmail.com>
2025-06-16 21:32:06 +0200

    Implementado lógica de login, conexión API, libreria ngx-sonner y alertas

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { FooterComponent } from "../../shared/footer/footer.component";
 import { NavComponent } from "../../shared/nav/nav.component";
+import { FormsModule } from '@angular/forms';
+
+import { UsersService } from '../../services/users.service';
+import { toast } from 'ngx-sonner';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, FooterComponent, NavComponent],
+  imports: [RouterLink, FooterComponent, NavComponent, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+usersService:any = inject(UsersService);
+router = inject(Router)
 
+  async getLogin(form:any){
+    try {
+    let response = await this.usersService.login(form.value);
+    if (response && response.token) {
+      this.router.navigate(['/dashboard']);
+      }
+  } 
+    
+    catch (error: any) {
+      toast.error(error.error.message)
+    
+  }
+}
 }
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Iuser } from '../interfaces/iuser.interface';
+import { lastValueFrom } from 'rxjs';
+
+type Response = {
+  message: string;
+  token: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
+private endpoint: string = "http://localhost:3000/api/users";
+private httpClient = inject(HttpClient);
 
-  constructor() { }
+login(user: Iuser): Promise<Response> {
+  return lastValueFrom(this.httpClient.post<Response>(`${this.endpoint}/login`, user));
+}
+
 }
