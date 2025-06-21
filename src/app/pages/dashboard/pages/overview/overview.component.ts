79a0b798bd9ac70e62c3a23416bcb7ae74eb3588
Daniel A. <1danielansias@gmail.com>
2025-06-21 09:31:39 +0200

    Configuración inicial del calendario

--- src/app/pages/dashboard/pages/overview/overview.component.ts
+++ src/app/pages/dashboard/pages/overview/overview.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
 
 @Component({
   selector: 'app-overview',
-  imports: [],
+  imports: [FullCalendarModule],
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.css'
 })
 export class OverviewComponent {
+  calendarOptions: CalendarOptions = {
+    initialView: 'dayGridMonth',
+    plugins: [dayGridPlugin, interactionPlugin],
+    dateClick: this.handleDateClick.bind(this),
+    events: [
+      { title: 'Reunión de equipo', date: '2025-06-25', backgroundColor: '#ff9f89', borderColor: '#ff9f89' },
+      { title: 'Deadline entrega', date: '2025-06-27', backgroundColor: '#ff9f89', borderColor: '#ff9f89' }
+    ]
+  };
 
+  handleDateClick(arg: any) {
+    alert('Haz clic en la fecha: ' + arg.dateStr);
+  }
 }
