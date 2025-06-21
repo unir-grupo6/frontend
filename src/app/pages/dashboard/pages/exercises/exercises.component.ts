949f426288eae000b7bff7cff5acb31b65b6ac8b
andresmir-94 <andresmirsampedro@gmail.com>
2025-06-21 16:21:24 +0200

    titulo, buscador y tap de dashboard/exercises

--- src/app/pages/dashboard/pages/exercises/exercises.component.ts
+++ src/app/pages/dashboard/pages/exercises/exercises.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-exercises',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './exercises.component.html',
   styleUrl: './exercises.component.css'
 })
 export class ExercisesComponent {
+categorias = ['Todos', 'Pecho', 'Espalda', 'Piernas', 'Brazos', 'Hombros'];
+categoriaSeleccionada = 'Todos';
 
+ActivarCategoria(categoria: string) {
+  this.categoriaSeleccionada = categoria;
+}
 }
