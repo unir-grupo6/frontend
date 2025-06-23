d4a19b1e8be26229a49ba7c59c0f022ac64ecaa6
andresmir-94 <andresmirsampedro@gmail.com>
2025-06-24 00:18:05 +0200

    Simulacion de card y modal

--- src/app/pages/dashboard/pages/exercises/exercises.component.ts
+++ src/app/pages/dashboard/pages/exercises/exercises.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { InstanceOptions, ModalInterface, ModalOptions, Modal } from 'flowbite';
 
 @Component({
   selector: 'app-exercises',
@@ -8,10 +9,45 @@ import { Component } from '@angular/core';
   styleUrl: './exercises.component.css'
 })
 export class ExercisesComponent {
-categorias = ['Todos', 'Pecho', 'Espalda', 'Piernas', 'Brazos', 'Hombros'];
+categorias = ['Todos', 'Fuerza', 'Aislamiento', 'Core', 'Cardio'];
 categoriaSeleccionada = 'Todos';
 
 ActivarCategoria(categoria: string) {
   this.categoriaSeleccionada = categoria;
 }
+
+
+ modal!: Modal;
+  readonly nivelesTotales = 5;
+  dificultadNivel: number = 3;
+  niveles: number[] = [];
+  
+
+  ngAfterViewInit() {
+    const modalEl = document.getElementById('modalEl');
+    if(modalEl){
+      this.modal = new Modal(modalEl, {
+        placement: 'bottom-right',
+        backdrop: 'dynamic',
+        backdropClasses: 'bg-gray-900/50 dark:bg-gray-900/80 fixed inset-0 z-40',
+        closable: true,
+        onHide: () => console.log('modal is hidden'),
+        onShow: () => console.log('modal is shown'),
+        onToggle: () => console.log('modal has been toggled'),
+      });
+    }
+  }
+
+
+  openModal() {
+    this.modal.show();
+  }
+
+  closeModal() {
+    this.modal.hide();
+  }
+
 }
+
+
+
