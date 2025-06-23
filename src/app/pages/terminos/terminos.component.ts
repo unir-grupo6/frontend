b7c5ac949ded411ef7df76a9738aa143dac419fd
Gabriel_Robles <gabrielroblescuev@gmail.com>
2025-06-23 22:02:58 +0200

    Texto añadidos

--- src/app/pages/terminos/terminos.component.ts
+++ src/app/pages/terminos/terminos.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavComponent } from "../../shared/nav/nav.component";
+import { FooterComponent } from "../../shared/footer/footer.component";
 
 @Component({
   selector: 'app-terminos',
-  imports: [],
+  imports: [NavComponent, FooterComponent],
   templateUrl: './terminos.component.html',
   styleUrl: './terminos.component.css'
 })
