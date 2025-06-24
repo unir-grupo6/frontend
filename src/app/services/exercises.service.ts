66f0c04bfa571d7dcb920b602718e2d1f403e713
andresmir-94 <andresmirsampedro@gmail.com>
2025-06-24 23:47:46 +0200

    Creado getAllExercises en exercisesService

--- src/app/services/exercises.service.ts
+++ src/app/services/exercises.service.ts
@@ -1,10 +1,15 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { IExercises } from '../interfaces/iexercises.interface';
+import { last, lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExercisesService {
   private endpoint: string = "https://rutina-go-backend.onrender.com/api";
+  private httpClient = inject(HttpClient);
 
-
+  getAllExercises() : Promise<IExercises[]> {
+    return lastValueFrom(this.httpClient.get<IExercises[]>(`${this.endpoint}/exercises`))}
 }
