c0b18a84dd587085e4672ef2d961c424552400ea
andresmir-94 <andresmirsampedro@gmail.com>
2025-07-01 17:15:53 +0200

    RouterLink añadido en 'Empezar ahora' dirigido a register, y cards home centradas una vez se hacen responsive

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { FooterComponent } from "../../shared/footer/footer.component";
 import { NavComponent } from "../../shared/nav/nav.component";
 import { CardComponentComponent } from '../../components/card-component/card-component.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [FooterComponent, NavComponent, CardComponentComponent],
+  imports: [FooterComponent, NavComponent, CardComponentComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
