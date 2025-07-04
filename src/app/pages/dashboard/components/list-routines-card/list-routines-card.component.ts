0531f5b1726f17a2d1be4be0bee8ec830e36d7c2
andresmir-94 <andresmirsampedro@gmail.com>
2025-07-04 18:37:12 +0200

    Realizado paginacion en el catalogo de rutinas filtradas y modificado los botones del catalogo de ejercicios para buscar unificacion de estilos, tambien realizado un pequeño cambio de estetica a la hora de mostrar las rutinas en el catalogo de rutinas filtradas

--- src/app/pages/dashboard/components/list-routines-card/list-routines-card.component.ts
+++ src/app/pages/dashboard/components/list-routines-card/list-routines-card.component.ts
@@ -1,24 +1,41 @@
-import { Component, Input } from '@angular/core';
-import { IRoutinesList, Ejercicio } from '../../../../interfaces/iroutines-list.interface';
-
-import { RouterLink } from '@angular/router';
+import { Component, Input, inject } from '@angular/core';
+import { IRoutinesList } from '../../../../interfaces/iroutines-list.interface';
+import { RoutinesService } from '../../../../services/routines.service';
+import { toast } from 'ngx-sonner';
 
 @Component({
   selector: 'app-list-routines-card',
-  imports: [RouterLink],
+  imports: [],
   templateUrl: './list-routines-card.component.html',
   styleUrl: './list-routines-card.component.css'
 })
 export class ListRoutinesCardComponent {
-@Input() rutinaList!: IRoutinesList;
+  @Input() rutinaList!: IRoutinesList;
 
+  routinesService = inject(RoutinesService);
+  
   selectedExerciseList: IRoutinesList | null = null;
 
-openModal(rutina: IRoutinesList) {
-  this.selectedExerciseList = rutina;
+  openModal(rutina: IRoutinesList) {
+    this.selectedExerciseList = rutina;
+  }
+
+  closeModal() {
+    this.selectedExerciseList = null;
+  }
+
+  async saveRoutine(id_rutina: number): Promise<void> {
+    try {
+      const response = await this.routinesService.savePublicRoutine(id_rutina);
+
+      if (response) {
+        toast.success('Rutina guardada en Mis Rutinas');
+      } else {
+        toast.error('Error al guardar la rutina');
+      }
+    } catch (error) {
+      toast.error('Error al guardar la rutina');
+    }
+  }
 }
 
-closeModal() {
-  this.selectedExerciseList = null;
-}
-}
