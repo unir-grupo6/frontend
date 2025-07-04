8e0fc4015e3df9622f6cc712fa95e5b6f7bb6ea1
Gabriel_Robles <gabrielroblescuev@gmail.com>
2025-07-04 23:28:05 +0200

    Modal eliminar rutina

--- src/app/pages/dashboard/components/detailed-routine-card/detailed-routine-card.component.ts
+++ src/app/pages/dashboard/components/detailed-routine-card/detailed-routine-card.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { RoutinesService } from '../../../../services/routines.service';
 import { toast } from 'ngx-sonner'
+import { Modal } from 'flowbite';
 
 @Component({
   selector: 'app-detailed-routine-card',
@@ -12,6 +13,45 @@ import { toast } from 'ngx-sonner'
 export class DetailedRoutineCardComponent {
   @Input() rutina: any;
   routinesService = inject(RoutinesService);
+
+  modal!: Modal;
+  private lastFocusedElement: HTMLElement | null = null;
+
+  ngAfterViewInit() {
+    const id = `modalEl-${this.rutina?.id}`;
+    const modalEl = document.getElementById(id);
+    if (modalEl) {
+      this.modal = new Modal(modalEl, {
+        placement: 'center',
+        backdrop: 'dynamic',
+        backdropClasses: 'bg-gray-900/50 fixed inset-0 z-40',
+        closable: true,
+      });
+    }
+  }
+
+  openModal(id: number) {
+    this.lastFocusedElement = document.activeElement as HTMLElement;
+    this.modal?.show();
+
+    const modalEl = document.getElementById(`modalEl-${id}`);
+    if (modalEl) {
+      const closeBtn = modalEl.querySelector(
+        'button[aria-label="Cerrar modal"]'
+      ) as HTMLElement;
+      if (closeBtn) {
+        closeBtn.focus();
+      }
+    }
+  }
+
+  closeModal() {
+    this.modal?.hide();
+    if (this.lastFocusedElement) {
+      this.lastFocusedElement.focus();
+    }
+  }
+
 
   getNombreDia(dia: number): string {
     const dias = [
@@ -32,6 +72,9 @@ export class DetailedRoutineCardComponent {
     try {
       await this.routinesService.deleteRoutine(id_rutina);
       toast.success('Rutina eliminada correctamente.')
+      setTimeout(() => {
+        window.location.reload();
+      }, 1000);
     } catch (error) {
       console.log("Error al eliminar la rutina con id: ", id)
     }
